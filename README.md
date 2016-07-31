6d32a6daa2e99205aafdec0553bb11027d5d805b
Kevin Kaske <kevinkaske@gmail.com>
2016-07-30 22:07:12 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # random-token
 A simple function to generate a random token
+
+## Install via composer
+Installation of pooch is through [composer](https://getcomposer.org). You simply need to create a directory and create a composer.json
+file with the following contents:
+```json
+{
+	"minimum-stability": "dev",
+	"require": {
+		"kevinkaske/random-token": "dev-master"
+	}
+}
+```
---
+++ libs/random_token.php
@@ -0,0 +1,22 @@
+<?
+function getToken($len = 40){
+  $token = '';
+  for ($i = 0; $i < $len; $i++) {
+    $ord = 0;
+    switch(rand(1,3)) {
+      case 1: // 0 - 9
+        $ord = rand(48,57);
+        break;
+      case 2: // A - Z
+        $ord = rand(65,90);
+        break;
+      case 3: // a - z
+        $ord = rand(97,112);
+        break;
+    }
+    $token = $token.chr($ord);
+  }
+
+  return $token;
+}
+?>
